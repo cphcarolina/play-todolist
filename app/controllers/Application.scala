02d0ed9f9c99869cec06070c31a148ea0398d4b5
Carolina <cphcarolina@gmail.com>
2014-09-16 23:40:06 +0200

    Creados listar y crear tareas (completos)
    Añadidos también comentarios sencillos en el código

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -10,18 +10,22 @@ import models.Task
 
 object Application extends Controller {
 
+   // Variable formulario
    val taskForm = Form(
       "label" -> nonEmptyText
    )
 
+   // Acceso a la raíz (índice)
   def index = Action {
     Redirect(routes.Application.tasks)
   }
 
+  // Listado de tareas y formulario de creación de las mismas
   def tasks = Action {
     Ok(views.html.index(Task.all(), taskForm))
   }
 
+  // Creación de una tarea (desde el template)
   def newTask = Action { implicit request =>
     taskForm.bindFromRequest.fold(
          errors => BadRequest(views.html.index(Task.all(), errors)),
@@ -32,5 +36,6 @@ object Application extends Controller {
       )
   }
 
+  // Eliminación de tareas (desde el template)
   def deleteTask(id: Long) = TODO
 }
--- app/models/Task.scala
+++ app/models/Task.scala
@@ -1,12 +1,39 @@
 package models
 
+import anorm._
+import anorm.SqlParser._
+
+import play.api.db._
+import play.api.Play.current
+
+// Definición de los parámetros de la tarea
 case class Task(id: Long, label: String)
 
+// DEfinición de los métodos de la tarea
 object Task {
-   
-   def all(): List[Task] = Nil
 
-   def create(label: String) {}
+   // Auxiliar para extraer las tareas de la BD (parser)
+   val task = {
+      get[Long] ("id") ~
+      get[String]("label") map {
+         case id~label => Task(id,label)
+      }
+   }
+   
+   // Método para obtener todas las tareas
+   def all(): List[Task] = DB.withConnection { implicit c =>
+      SQL("Select * from task").as(task *) 
+      // (task *) sirve para crear tantas tareas como líneas en la tabla existan
+   }
+
+   // Método para crear tareas
+   def create(label: String) {
+      DB.withConnection { implicit c =>
+         SQL("insert into task (label) values ({label})").on(
+            'label -> label
+         ).executeUpdate()
+      }
+   }
 
    def delete(id: Long) {}
 }
